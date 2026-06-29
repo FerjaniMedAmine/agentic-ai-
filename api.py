b6d82288d392b6f81911449fc35153ada7898f7f
FerjaniMedAmine <amineferjani961@gmail.com>
2026-06-29 19:04:24 +0100

    wrap agent with fastAPI API

---
+++ api.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from llm_config import get_llm, clean_up, setup_cache
+from tools import all_tools
+from langchain.agents import create_agent
+
+
+
+setup_cache()
+llm,llama_process =get_llm()
+agent = create_agent(
+    model=llm,
+    tools=all_tools,
+    system_prompt="You are a helpful assistant"
+)
+
+
+app = FastAPI()
+messages =[]
+
+class ChatRequest(BaseModel):
+    message:str
+
+
+@app.post("/chat")
+def chat(req:ChatRequest):
+    messages.append({"role": "user", "content": req.message})
+    result =agent.invoke({"messages": messages})
+    response = result["messages"][-1].content
+    messages.append({"role": "assistant", "content": response})
+    return{
+        "response":response
+    }
+
